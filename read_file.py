2d2643c209591e4a4e97b2a0a1687e16f8b9e8ef
alexioso <alexbraksator@gmail.com>
2018-05-07 16:46:08 -0700

    fixed

--- read_file.py
+++ read_file.py
@@ -9,7 +9,7 @@ def main():
 	file_path = sys.argv[1]
 	binary_vector = pd.read_csv(file_path,header=None,nrows=1).values.tolist()[0]
 	df = pd.read_csv(file_path,skiprows=1,header=None)
-	for i in range(len(binary_vector)):
+	for i in range(len(binary_vector)-1,-1,-1):
 		val = binary_vector[i]
 		if val == 0:
 			df = df.drop(i,axis=1)
